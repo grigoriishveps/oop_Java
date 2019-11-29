db4441e3670fc1eef77536809ef8365f8a647dd4
grigoriishveps <grigorii.shveps@gmail.com>
2019-11-29 13:31:47 +0300

    6 lab 1.2

--- src/main/java/Laba1.java
+++ src/main/java/Laba1.java
@@ -1,8 +1,16 @@
 import java.util.*;
 import laba.*;
+
+import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.LogManager;
+
 public class Laba1 {
+
     public static void main(String[] args) {
         //Scanner scanner = new Scanner(System.in);
         new GraphicInterface();
+        Logger logger = LogManager.getLogger(GraphicInterface.class);
+        logger.info("Hello World");
+
     }
 }
